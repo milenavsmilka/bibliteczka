a827a33b012fa1af33e2a7b6367e1643f0bff2a1
milenavsmilka <milena.milena16@onet.pl>
2024-04-30 11:39:32 +0200

    wyrzucenie niepotrzebnych bibliotek

--- bibliteczka/lib/panels/News/NewBooksScreen.dart
+++ bibliteczka/lib/panels/News/NewBooksScreen.dart
@@ -1,11 +1,6 @@
-import 'dart:convert';
-
 import 'package:biblioteczka/panels/Tools/LoadingScreen.dart';
 import 'package:biblioteczka/panels/functions.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../styles/strings.dart';
